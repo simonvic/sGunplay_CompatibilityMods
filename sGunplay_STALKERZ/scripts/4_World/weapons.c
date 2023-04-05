70fc30f95b809bd0d4db73f33c1831f1a0a8f32a
simonvic <simonvic.dev@gmail.com>
2023-04-06 00:07:50 +0200

    fix(stalkerz): wrong recoil type for weapons

---
+++ sGunplay_STALKERZ/scripts/4_World/weapons.c
@@ -0,0 +1,35 @@
+modded class Aug_A3_Base {
+	override RecoilBase SpawnRecoilObject() {
+		return new AUGRecoil(this);
+	}
+}
+modded class AK5C_Base {
+	override RecoilBase SpawnRecoilObject() {
+		return new Ak101Recoil(this);
+	}
+}
+modded class sz_OTs14_9x39_Base {
+	override RecoilBase SpawnRecoilObject() {
+		return new VSSRecoil(this);
+	}
+}
+modded class sz_Saiga12_Base {
+	override RecoilBase SpawnRecoilObject() {
+		return new SiagaRecoil(this);
+	}
+}
+modded class sz_Stoner63A_Base {
+	override RecoilBase SpawnRecoilObject() {
+		return new M4a1Recoil(this);
+	}
+}
+modded class sz_FAL_Base {
+	override RecoilBase SpawnRecoilObject() {
+		return new FALRecoil(this);
+	}
+}
+modded class Ultimax100_Base {
+	override RecoilBase SpawnRecoilObject() {
+		return new AUGRecoil(this);
+	}
+}
