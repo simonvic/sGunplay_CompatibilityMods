e11017f183f9d955b9d4567d7cd603d7bed3d220
simonvic <simonvic.dev@gmail.com>
2023-11-16 02:14:03 +0100

    fix(flava): update attachment stats

--- sGunplay_FLAVA/config.cpp
+++ sGunplay_FLAVA/config.cpp
@@ -33,57 +33,13 @@ class CfgPatches {
 		weapons[] = {};
 		requiredVersion = 0.1;
 		requiredAddons[] = {
-			"sGunplay",
-			"script_a", 
-			"TF_AK_Zenit_PT1Bttstck",
-			"TF_AN94_Bttstck",
-			"TF_Attachments",
-			"TF_MK20_Buttstock",
-			"TF_Grips",
-			"TF_ADD_SCAR_Handguard",
-			"TF_AKSu_Handguards",
-			"TF_AKS74U_CAAHndgrd_RIS",
-			"TF_AKSu_Handguards",
-			"TF_AK_Hndgrd",
-			"TF_Handguard_CMMG_15",
-			"TF_Handguard_CMMG_9",
-			"TF_Handguard_M4",
-			"TF_MREX_Handguard",
-			"TF_STD_SCAR_Handguard",
-			"TF_OTs14_Groza_Muzzle",
-			"TF_PistolGrips",
-			"TF_AK_Bttstck",
-			"TF_M4_B5PrecisionBttstck",
-			"TF_M4_FABDGLCoreBttstck",
-			"TF_M4_MagpulPRSGen3Bttstck",
-			"TF_PKM_CNCC39Bttstck",
-			"TF_PKM_ZenitPT2Bttstck",
-			"TF_PKM_WoodBttstck",
-			"TF_PKP_PlasticBttstck",
-			"TF_AR10_KACHndgrd_RIS",
-			"TF_AR10_LancerHndgrd",
-			"TF_AR10_NoveskeHndgrd",
-			"TF_AR10_URXHndgrd",
-			"TF_Handguard_M16",
-			"TF_PKM_Handguard",
-			"TF_PKM_6ch43Compensator",
-			"TF_PKM_6ch63Compensator",
-			"TF_PKM_6ch76Compensator",
-			"TF_PKM_6p20_Compensator",
-			"TF_PKM_DTK1_Compensator",
-			"TF_PKM_SAICompensator",
-			"TF_PistolGrips_B",
-			"TF_Elcan_SpecterDR",
-			"TF_EOTech_XPS",
-			"TF_HAMR",
-			"TF_OTs_14_Groza_Optic",
-			"TF_AK_MVRIBttstck"
+			"sGunplay"
 		};
 	};
 };
 
 class cfgVehicles {
-	
+	///////////////////////////////////////////////////////////////////// OPTICS
 	class ItemOptics;
 	class TF_OTs_14_GrozaOptic : ItemOptics {
 		s_isFullscreen = 1;
@@ -123,7 +79,6 @@ class cfgVehicles {
 			discretefov[] = {}; //sorry no double zoom :(
 		};
 	};
-
 	class TF_EOTechXPSOptic_Base;
 	class TF_EOTechXPSOpticUp_Black : TF_EOTechXPSOptic_Base {
 		s_pipRadius = 0.7;
@@ -132,114 +87,8 @@ class cfgVehicles {
 		s_pipChromAber = 0.2;
 	};
 	
-
-	class ItemSuppressor;
-	class TF_PKM_SAICompensator : ItemSuppressor {
-		s_recoilControlMisalignmentX = 0.45;
-		s_recoilControlMisalignmentY = 0.05;
-	};
-	class TF_AK_ZenitDTK1Compensator : ItemSuppressor {
-		s_recoilControlMisalignmentX = 0.45;
-		s_recoilControlMisalignmentY = 0.05;
-	};
-	class TF_AK_6p20Compensator : ItemSuppressor {
-		s_recoilControlMisalignmentX = 0.45;
-		s_recoilControlMisalignmentY = 0.05;
-	};
-	class TF_PKM_6ch76Compensator : ItemSuppressor {
-		s_recoilControlMisalignmentX = 0.45;
-		s_recoilControlMisalignmentY = 0.05;
-	};
-	class TF_PKM_6ch63Compensator : ItemSuppressor {
-		s_recoilControlMisalignmentX = 0.45;
-		s_recoilControlMisalignmentY = 0.05;
-	};
-	class TF_PKM_6ch43Compensator : ItemSuppressor {
-		s_recoilControlMisalignmentX = 0.45;
-		s_recoilControlMisalignmentY = 0.05;
-	};
-
 	class Inventory_Base;
-	class TF_AR_Grip_Base : Inventory_Base {
-		s_recoilControlMisalignmentX = 0.05;
-		s_recoilControlMisalignmentY = 0.05;
-	};
-	class TF_AK_Grip_Base : Inventory_Base {
-		s_recoilControlMisalignmentX = 0.05;
-		s_recoilControlMisalignmentY = 0.05;
-	};
-	class TF_PKP_CNCC46Hndgrd_RIS : Inventory_Base {
-		s_recoilControlStabilityX = 0.20;
-		s_recoilControlStabilityY = 0.20;
-		s_recoilControlMisalignmentX = 0.55;
-		s_recoilControlMisalignmentY = 0.50;
-	};
-	class TF_PKM_ZenitB50Hndgrd_RIS : Inventory_Base {
-		s_recoilControlStabilityX = 0.20;
-		s_recoilControlStabilityY = 0.20;
-		s_recoilControlMisalignmentX = 0.55;
-		s_recoilControlMisalignmentY = 0.50;
-	};
-	class TF_M16_KACHndgrd_RIS_Base : Inventory_Base {
-		s_recoilControlStabilityX = 0.20;
-		s_recoilControlStabilityY = 0.20;
-		s_recoilControlMisalignmentX = 0.55;
-		s_recoilControlMisalignmentY = 0.50;
-	};
-	class TF_AR10_URXHndgrd_MLOCK : Inventory_Base {
-		s_recoilControlStabilityX = 0.20;
-		s_recoilControlStabilityY = 0.20;
-		s_recoilControlMisalignmentX = 0.55;
-		s_recoilControlMisalignmentY = 0.50;
-	};
-	class TF_AR10_NoveskeHndgrd_Short_RIS : Inventory_Base {
-		s_recoilControlStabilityX = 0.20;
-		s_recoilControlStabilityY = 0.20;
-		s_recoilControlMisalignmentX = 0.55;
-		s_recoilControlMisalignmentY = 0.50;
-	};
-	class TF_AR10_NoveskeHndgrd_Long_RIS : Inventory_Base {
-		s_recoilControlStabilityX = 0.20;
-		s_recoilControlStabilityY = 0.20;
-		s_recoilControlMisalignmentX = 0.55;
-		s_recoilControlMisalignmentY = 0.50;
-	};
-	class TF_AR10_LancerHndgrd_MLOCK : Inventory_Base {
-		s_recoilControlStabilityX = 0.20;
-		s_recoilControlStabilityY = 0.20;
-		s_recoilControlMisalignmentX = 0.55;
-		s_recoilControlMisalignmentY = 0.50;
-	};
-	class TF_AR10_KACHndgrd_RIS : Inventory_Base {
-		s_recoilControlStabilityX = 0.20;
-		s_recoilControlStabilityY = 0.20;
-		s_recoilControlMisalignmentX = 0.55;
-		s_recoilControlMisalignmentY = 0.50;
-	};
-	class TF_AK_ZenitB10MB19Hndgrd_RIS : Inventory_Base {
-		s_recoilControlStabilityX = 0.20;
-		s_recoilControlStabilityY = 0.20;
-		s_recoilControlMisalignmentX = 0.55;
-		s_recoilControlMisalignmentY = 0.50;
-	};
-	class TF_AK_MagpulZhukovHndgrdU_MLOCK : Inventory_Base {
-		s_recoilControlStabilityX = 0.20;
-		s_recoilControlStabilityY = 0.20;
-		s_recoilControlMisalignmentX = 0.55;
-		s_recoilControlMisalignmentY = 0.50;
-	};
-	class TF_AK_MagpulMOEHndgrd_MLOCK : Inventory_Base {
-		s_recoilControlStabilityX = 0.20;
-		s_recoilControlStabilityY = 0.20;
-		s_recoilControlMisalignmentX = 0.55;
-		s_recoilControlMisalignmentY = 0.50;
-	};
-	class TF_ZenitRK1B25U_RIS_Grip : Inventory_Base {
-		s_recoilControlStabilityX = 0.15;
-		s_recoilControlStabilityY = 0.15;
-		s_recoilControlMisalignmentX = 0.30;
-		s_recoilControlMisalignmentY = 0.25;
-	};
+	///////////////////////////////////////////////////////////////// BUTTSTOCKS
 	class TF_PKP_PlasticBttstck : Inventory_Base {
 		s_recoilControlStabilityX = 0.45;
 		s_recoilControlStabilityY = 0.45;
@@ -366,33 +215,73 @@ class cfgVehicles {
 		s_recoilControlMisalignmentY = 0.25;
 		s_recoilControlKick = 0.75;
 	};
-	class TF_SAW_RIS_Grip_Base : Inventory_Base {
-		s_recoilControlMisalignmentX = 0.05;
-		s_recoilControlMisalignmentY = 0.05;
-	};
-	class TF_AFG_RIS_Grip_Base : Inventory_Base {
+
+	///////////////////////////////////////////////////////////////// HANDGUARDS
+	class TF_PKP_CNCC46Hndgrd_RIS : Inventory_Base {
 		s_recoilControlStabilityX = 0.20;
 		s_recoilControlStabilityY = 0.20;
-		s_recoilControlMisalignmentX = 0.35;
-		s_recoilControlMisalignmentY = 0.30;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
 	};
-	class TF_AFG_MLOCK_Grip_Base : Inventory_Base {
+	class TF_PKM_ZenitB50Hndgrd_RIS : Inventory_Base {
 		s_recoilControlStabilityX = 0.20;
 		s_recoilControlStabilityY = 0.20;
-		s_recoilControlMisalignmentX = 0.35;
-		s_recoilControlMisalignmentY = 0.30;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
 	};
-	class TF_BCM_MLOCK_Grip_Base : Inventory_Base {
-		s_recoilControlStabilityX = 0.15;
-		s_recoilControlStabilityY = 0.15;
-		s_recoilControlMisalignmentX = 0.30;
-		s_recoilControlMisalignmentY = 0.25;
+	class TF_M16_KACHndgrd_RIS_Base : Inventory_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
 	};
-	class TF_RVG_RIS_Grip_Base : Inventory_Base {
-		s_recoilControlStabilityX = 0.15;
-		s_recoilControlStabilityY = 0.15;
-		s_recoilControlMisalignmentX = 0.30;
-		s_recoilControlMisalignmentY = 0.25;
+	class TF_AR10_URXHndgrd_MLOCK : Inventory_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
+	};
+	class TF_AR10_NoveskeHndgrd_Short_RIS : Inventory_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
+	};
+	class TF_AR10_NoveskeHndgrd_Long_RIS : Inventory_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
+	};
+	class TF_AR10_LancerHndgrd_MLOCK : Inventory_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
+	};
+	class TF_AR10_KACHndgrd_RIS : Inventory_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
+	};
+	class TF_AK_ZenitB10MB19Hndgrd_RIS : Inventory_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
+	};
+	class TF_AK_MagpulZhukovHndgrdU_MLOCK : Inventory_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
+	};
+	class TF_AK_MagpulMOEHndgrd_MLOCK : Inventory_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
 	};
 	class TF_SCAR_ADDHndgrd_Base : Inventory_Base {
 		s_recoilControlStabilityX = 0.20;
@@ -490,10 +379,213 @@ class cfgVehicles {
 		s_recoilControlMisalignmentX = 0.55;
 		s_recoilControlMisalignmentY = 0.50;
 	};
+	
+	////////////////////////////////////////////////////////////////////// GRIPS
+	class TF_ZenitRK1B25U_RIS_Grip : Inventory_Base {
+		s_recoilControlStabilityX = 0.15;
+		s_recoilControlStabilityY = 0.15;
+		s_recoilControlMisalignmentX = 0.05;
+		s_recoilControlMisalignmentY = 0.10;
+	};
+	class TF_AR_Grip_Base : Inventory_Base {
+		s_recoilControlMisalignmentX = 0.05;
+		s_recoilControlMisalignmentY = 0.05;
+	};
+	class TF_AK_Grip_Base : Inventory_Base {
+		s_recoilControlMisalignmentX = 0.05;
+		s_recoilControlMisalignmentY = 0.05;
+	};
+	class TF_SAW_RIS_Grip_Base : Inventory_Base {
+		s_recoilControlMisalignmentX = 0.05;
+		s_recoilControlMisalignmentY = 0.05;
+	};
+	class TF_AFG_RIS_Grip_Base : Inventory_Base {
+		s_recoilControlStabilityX = 0.15;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.05;
+		s_recoilControlMisalignmentY = 0.10;
+	};
+	class TF_AFG_MLOCK_Grip_Base : Inventory_Base {
+		s_recoilControlStabilityX = 0.15;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.05;
+		s_recoilControlMisalignmentY = 0.10;
+	};
+	class TF_BCM_MLOCK_Grip_Base : Inventory_Base {
+		s_recoilControlStabilityX = 0.15;
+		s_recoilControlStabilityY = 0.15;
+		s_recoilControlMisalignmentX = 0.05;
+		s_recoilControlMisalignmentY = 0.10;
+	};
+	class TF_RVG_RIS_Grip_Base : Inventory_Base {
+		s_recoilControlStabilityX = 0.15;
+		s_recoilControlStabilityY = 0.15;
+		s_recoilControlMisalignmentX = 0.05;
+		s_recoilControlMisalignmentY = 0.10;
+	};
+
+	/////////////////////////////////////////////////////////////// COMPENSATORS
+	class ItemSuppressor;
 	class TF_OTs_14_Groza_Barrel_Grip : ItemSuppressor {
 		s_recoilControlStabilityX = 0.20;
 		s_recoilControlStabilityY = 0.20;
 		s_recoilControlMisalignmentX = 0.55;
 		s_recoilControlMisalignmentY = 0.50;
 	};
+	class TF_PKM_SAICompensator : ItemSuppressor {
+		s_recoilControlStabilityX = 0.15;
+		s_recoilControlStabilityY = 0.05;
+		s_recoilControlMisalignmentX = 0.10;
+		s_recoilControlMisalignmentY = 0.05;
+	};
+	class TF_AK_ZenitDTK1Compensator : ItemSuppressor {
+		s_recoilControlStabilityX = 0.15;
+		s_recoilControlStabilityY = 0.05;
+		s_recoilControlMisalignmentX = 0.10;
+		s_recoilControlMisalignmentY = 0.05;
+	};
+	class TF_AK_6p20Compensator : ItemSuppressor {
+		s_recoilControlStabilityX = 0.15;
+		s_recoilControlStabilityY = 0.05;
+		s_recoilControlMisalignmentX = 0.10;
+		s_recoilControlMisalignmentY = 0.05;
+	};
+	class TF_PKM_6ch76Compensator : ItemSuppressor {
+		s_recoilControlStabilityX = 0.15;
+		s_recoilControlStabilityY = 0.05;
+		s_recoilControlMisalignmentX = 0.10;
+		s_recoilControlMisalignmentY = 0.05;
+	};
+	class TF_PKM_6ch63Compensator : ItemSuppressor {
+		s_recoilControlStabilityX = 0.15;
+		s_recoilControlStabilityY = 0.05;
+		s_recoilControlMisalignmentX = 0.10;
+		s_recoilControlMisalignmentY = 0.05;
+	};
+	class TF_PKM_6ch43Compensator : ItemSuppressor {
+		s_recoilControlStabilityX = 0.15;
+		s_recoilControlStabilityY = 0.05;
+		s_recoilControlMisalignmentX = 0.10;
+		s_recoilControlMisalignmentY = 0.05;
+	};
+};
+
+class CfgWeapons {
+	class Rifle_Base;
+	class TF_AN94_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
+	};
+	class TF_SCAR_L_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
+	};
+	class TF_SCAR_L_CQC_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
+	};
+	class TF_SCAR_H_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
+	};
+	class TF_SCAR_H_CQC_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
+	};
+	class TF_SCAR_20_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
+	};
+	class TF_OTs_14_Groza_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.70;
+		s_recoilControlStabilityY = 0.70;
+		s_recoilControlMisalignmentX = 0.70;
+		s_recoilControlMisalignmentY = 0.75;
+		s_recoilControlKick = 0.70;
+	};
+
+	class TF_AEK999_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
+	};
+	class TF_M240_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.70;
+		s_recoilControlStabilityY = 0.70;
+		s_recoilControlMisalignmentX = 0.70;
+		s_recoilControlMisalignmentY = 0.75;
+		s_recoilControlKick = 0.70;
+	};
+	class TF_M249_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.70;
+		s_recoilControlStabilityY = 0.70;
+		s_recoilControlMisalignmentX = 0.70;
+		s_recoilControlMisalignmentY = 0.75;
+		s_recoilControlKick = 0.70;
+	};
+
+	class TF_M1A_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.65;
+		s_recoilControlStabilityY = 0.70;
+		s_recoilControlMisalignmentX = 0.75;
+		s_recoilControlMisalignmentY = 0.80;
+		s_recoilControlKick = 0.75;
+	};
+	class TF_M14_DMR_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.65;
+		s_recoilControlStabilityY = 0.70;
+		s_recoilControlMisalignmentX = 0.75;
+		s_recoilControlMisalignmentY = 0.80;
+		s_recoilControlKick = 0.75;
+	};
+	class TF_M14_EBR_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.65;
+		s_recoilControlStabilityY = 0.70;
+		s_recoilControlMisalignmentX = 0.75;
+		s_recoilControlMisalignmentY = 0.80;
+		s_recoilControlKick = 0.75;
+	};
+	class TF_M14_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.65;
+		s_recoilControlStabilityY = 0.70;
+		s_recoilControlMisalignmentX = 0.75;
+		s_recoilControlMisalignmentY = 0.80;
+		s_recoilControlKick = 0.75;
+	};
+	class TF_MAR_10_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.50;
+	};
+	class TF_SVCh_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.65;
+		s_recoilControlStabilityY = 0.70;
+		s_recoilControlMisalignmentX = 0.75;
+		s_recoilControlMisalignmentY = 0.80;
+		s_recoilControlKick = 0.75;
+	};
+	
+	class BoltActionRifle_Base;
+	class TF_VPO190_Base : BoltActionRifle_Base {
+		s_recoilControlStabilityX = 0.65;
+		s_recoilControlStabilityY = 0.65;
+		s_recoilControlMisalignmentX = 0.70;
+		s_recoilControlMisalignmentY = 0.70;
+		s_recoilControlKick = 0.70;
+	};
+	
 };
