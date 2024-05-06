609c1ddd5db5cbee6d048139bb222e9e2780da51
simonvic <simonvic.dev@gmail.com>
2024-05-06 18:21:26 +0200

    fix(mmio): update recoils and stats

--- sGunplay_MMIO/config.cpp
+++ sGunplay_MMIO/config.cpp
@@ -5,22 +5,9 @@ class CfgPatches {
 		requiredVersion = 0.1;
 		requiredAddons[] = {
 			"sGunplay",
-			"Mass_Guns",
+			"MassGuns_Muzzles",
 			"MassGuns_Optics",
-			"MassGuns_Support",
-			"MassGuns_Weapons_ACR",
-			"MassGuns_Weapons_AK9",
-			"MassGuns_Weapons_AN94",
-			"MassGuns_Weapons_HK416",
-			"MassGuns_Weapons_M19",
-			"MassGuns_Weapons_M16",
-			"MassGuns_Weapons_M417",
-			"MassGuns_Weapons_PredatAR",
-			"MassGuns_Weapons_RPK",
-			"MassGuns_Weapons_Type81",
-			"MassGuns_Weapons_WolfAR",
-			"MassGuns_Weapons_PPSh",
-			"MassGuns_Muzzles"
+			"MassGuns_Support"
 		};
 	};
 };
@@ -54,160 +41,266 @@ class CfgMods {
 };
 
 class cfgWeapons {
-
-	
 	class Rifle_Base;
-	class MassPPSH41_Base : Rifle_Base {
-		s_recoilControlStabilityX = 0.65;
-		s_recoilControlStabilityY = 0.65;
-		s_recoilControlMisalignmentX = 0.50;
-		s_recoilControlMisalignmentY = 0.70;
-		s_recoilControlKick = 0.70;
-	};
-	class MassMP40_Base : Rifle_Base {
-		s_recoilControlStabilityX = 0.65;
-		s_recoilControlStabilityY = 0.65;
-		s_recoilControlMisalignmentX = 0.50;
-		s_recoilControlMisalignmentY = 0.70;
-		s_recoilControlKick = 0.70;
-	};
-	class MassType81_Base : Rifle_Base {
-		s_recoilControlStabilityX = 0.65;
-		s_recoilControlStabilityY = 0.65;
-		s_recoilControlMisalignmentX = 0.70;
-		s_recoilControlMisalignmentY = 0.70;
-		s_recoilControlKick = 0.70;
-	};
-	class MassM417_Base : Rifle_Base {
-		s_recoilControlStabilityX = 0.25;
-		s_recoilControlStabilityY = 0.05;
-		s_recoilControlMisalignmentX = 0.55;
-		s_recoilControlMisalignmentY = 0.45;
-	};
 	class Mass_AN94_Base : Rifle_Base {
-		s_recoilControlStabilityX = 0.65;
-		s_recoilControlStabilityY = 0.65;
-		s_recoilControlMisalignmentX = 0.70;
-		s_recoilControlMisalignmentY = 0.70;
-		s_recoilControlKick = 0.70;
+		s_recoilControlStabilityX = 0.60;
+		s_recoilControlStabilityY = 0.60;
+		s_recoilControlMisalignmentX = 0.60;
+		s_recoilControlMisalignmentY = 0.60;
+		s_recoilControlKick = 0.60;
 	};
-	class MassM19_Base : Rifle_Base {
-		s_recoilControlStabilityX = 0.65;
-		s_recoilControlStabilityY = 0.65;
-		s_recoilControlMisalignmentX = 0.70;
-		s_recoilControlMisalignmentY = 0.70;
-		s_recoilControlKick = 0.70;
-	};
-	class AKS74U;
-	class Mass_AK9 : AKS74U {
-		s_recoilControlStabilityX = 0.00;
-		s_recoilControlStabilityY = 0.00;
-		s_recoilControlMisalignmentX = 0.00;
-		s_recoilControlMisalignmentY = 0.00;
-		s_recoilControlKick = 0.00;
-	};
-	class AK74;
-	class Mass_RPK74_Base : AK74 {
-		s_recoilControlStabilityX = 0.15;
-		s_recoilControlStabilityY = 0.20;
-		s_recoilControlMisalignmentX = 0.00;
-		s_recoilControlMisalignmentY = 0.00;
-		s_recoilControlKick = 0.70;
+	class MassB52_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.60;
+		s_recoilControlStabilityY = 0.60;
+		s_recoilControlMisalignmentX = 0.60;
+		s_recoilControlMisalignmentY = 0.60;
+		s_recoilControlKick = 0.60;
 	};
 	class M16A2;
 	class Mass_HK416_Base : M16A2 {
-		s_recoilControlStabilityX = 0.25;
-		s_recoilControlStabilityY = 0.05;
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
 		s_recoilControlMisalignmentX = 0.55;
 		s_recoilControlMisalignmentY = 0.45;
+	};
+	class MassKivaari_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.60;
+		s_recoilControlStabilityY = 0.60;
+		s_recoilControlMisalignmentX = 0.60;
+		s_recoilControlMisalignmentY = 0.60;
+		s_recoilControlKick = 0.60;
 	};
 	class M4A1;
-	class MassAR15_Base : M4A1 {
-		s_recoilControlStabilityX = 0.65;
-		s_recoilControlStabilityY = 0.65;
-		s_recoilControlMisalignmentX = 0.70;
-		s_recoilControlMisalignmentY = 0.70;
-		s_recoilControlKick = 0.70;
+	class MassM16_Base : M4A1 {
+		s_recoilControlStabilityX = 0.60;
+		s_recoilControlStabilityY = 0.60;
+		s_recoilControlMisalignmentX = 0.60;
+		s_recoilControlMisalignmentY = 0.60;
+		s_recoilControlKick = 0.60;
 	};
-	class MassAR15DMR_Base : M4A1 {
-		s_recoilControlStabilityX = 0.25;
-		s_recoilControlStabilityY = 0.05;
+	class CZ527;
+	class MassM24_Base : CZ527 {
+		s_recoilControlStabilityX = 0.60;
+		s_recoilControlStabilityY = 0.60;
+		s_recoilControlMisalignmentX = 0.60;
+		s_recoilControlMisalignmentY = 0.60;
+		s_recoilControlKick = 0.60;
+	};
+	class Mass_M98B_Base : CZ527 {
+		s_recoilControlStabilityX = 0.60;
+		s_recoilControlStabilityY = 0.60;
+		s_recoilControlMisalignmentX = 0.60;
+		s_recoilControlMisalignmentY = 0.60;
+		s_recoilControlKick = 0.60;
+	};
+	class BoltActionRifle_Base;
+	class MassM300_Base : BoltActionRifle_Base {
+		s_recoilControlStabilityX = 0.60;
+		s_recoilControlStabilityY = 0.60;
+		s_recoilControlMisalignmentX = 0.60;
+		s_recoilControlMisalignmentY = 0.60;
+		s_recoilControlKick = 0.60;
+	};
+	class MassM417_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
 		s_recoilControlMisalignmentX = 0.55;
 		s_recoilControlMisalignmentY = 0.45;
 	};
-	class MassM16_Base : M4A1 {
-		s_recoilControlStabilityX = 0.65;
-		s_recoilControlStabilityY = 0.65;
-		s_recoilControlMisalignmentX = 0.70;
-		s_recoilControlMisalignmentY = 0.70;
-		s_recoilControlKick = 0.70;
+	class MassMini14_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.60;
+		s_recoilControlStabilityY = 0.60;
+		s_recoilControlMisalignmentX = 0.60;
+		s_recoilControlMisalignmentY = 0.60;
+		s_recoilControlKick = 0.60;
 	};
-	class Mass_ACR_Base : M4A1 {
-		s_recoilControlStabilityX = 0.65;
-		s_recoilControlStabilityY = 0.65;
-		s_recoilControlMisalignmentX = 0.70;
-		s_recoilControlMisalignmentY = 0.70;
-		s_recoilControlKick = 0.70;
+	class MassMP40_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.60;
+		s_recoilControlStabilityY = 0.60;
+		s_recoilControlMisalignmentX = 0.60;
+		s_recoilControlMisalignmentY = 0.60;
+		s_recoilControlKick = 0.60;
+	};
+	class MassMP153_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.60;
+		s_recoilControlStabilityY = 0.60;
+		s_recoilControlMisalignmentX = 0.60;
+		s_recoilControlMisalignmentY = 0.60;
+		s_recoilControlKick = 0.60;
+	};
+	class Mass_PKM_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.60;
+		s_recoilControlStabilityY = 0.60;
+		s_recoilControlMisalignmentX = 0.60;
+		s_recoilControlMisalignmentY = 0.60;
+		s_recoilControlKick = 0.60;
+	};
+	class MassPPSH41_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.60;
+		s_recoilControlStabilityY = 0.60;
+		s_recoilControlMisalignmentX = 0.60;
+		s_recoilControlMisalignmentY = 0.60;
+		s_recoilControlKick = 0.60;
+	};
+	class MassAR15DMR_Base : M4A1 {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.45;
+	};
+	class AK74;
+	class Mass_RPK74_Base : AK74 {
+		s_recoilControlStabilityX = 0.40;
+		s_recoilControlStabilityY = 0.40;
+		s_recoilControlMisalignmentX = 0.05;
+		s_recoilControlMisalignmentY = 0.15;
+		s_recoilControlKick = 0.60;
+	};
+	class Scout;
+	class Mass_RugerPrecision_Base : Scout {
+		s_recoilControlStabilityX = 0.60;
+		s_recoilControlStabilityY = 0.60;
+		s_recoilControlMisalignmentX = 0.60;
+		s_recoilControlMisalignmentY = 0.60;
+		s_recoilControlKick = 0.60;
+	};
+	class Mass_SPAS12_Base : MassMP153_Base {
+		s_recoilControlStabilityX = 0.20;
+		s_recoilControlStabilityY = 0.20;
+		s_recoilControlMisalignmentX = 0.55;
+		s_recoilControlMisalignmentY = 0.45;
+	};
+	class Izh18;
+	class Stevens_Base : Izh18 {
+		s_recoilControlStabilityX = 0.60;
+		s_recoilControlStabilityY = 0.60;
+		s_recoilControlMisalignmentX = 0.60;
+		s_recoilControlMisalignmentY = 0.60;
+		s_recoilControlKick = 0.60;
+	};
+	class MassSVT40_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.60;
+		s_recoilControlStabilityY = 0.60;
+		s_recoilControlMisalignmentX = 0.60;
+		s_recoilControlMisalignmentY = 0.60;
+		s_recoilControlKick = 0.60;
+	};
+	class Mass_SVU_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.60;
+		s_recoilControlStabilityY = 0.60;
+		s_recoilControlMisalignmentX = 0.60;
+		s_recoilControlMisalignmentY = 0.60;
+		s_recoilControlKick = 0.60;
+	};
+	class MassType81_Base : Rifle_Base {
+		s_recoilControlStabilityX = 0.60;
+		s_recoilControlStabilityY = 0.60;
+		s_recoilControlMisalignmentX = 0.60;
+		s_recoilControlMisalignmentY = 0.60;
+		s_recoilControlKick = 0.60;
 	};
 };
 
 class cfgVehicles{
-	class Inventory_Base;
-	class MassSPAS12Stock : Inventory_Base {
-		s_recoilControlStabilityX = 0.10;
-		s_recoilControlStabilityY = 0.10;
-		s_recoilControlMisalignmentX = 0.10;
-		s_recoilControlMisalignmentY = 0.20;
-		s_recoilControlKick = 0.60;
-	};
 
-	class ItemSuppressor;
-	class MassMuzzle338 : ItemSuppressor {
-		s_recoilControlMisalignmentX = 0.45;
-		s_recoilControlMisalignmentY = 0.05;
-	};
-	class MassMuzzle556 : ItemSuppressor {
-		s_recoilControlMisalignmentX = 0.45;
-		s_recoilControlMisalignmentY = 0.05;
-	};
-	class MassMuzzle762 : ItemSuppressor {
-		s_recoilControlMisalignmentX = 0.45;
-		s_recoilControlMisalignmentY = 0.05;
-	};
-	class MassMuzzleComp556 : ItemSuppressor {
-		s_recoilControlMisalignmentX = 0.45;
-		s_recoilControlMisalignmentY = 0.05;
-	};
-	class MassMuzzlePistol : ItemSuppressor {
-		s_recoilControlMisalignmentX = 0.45;
-		s_recoilControlMisalignmentY = 0.05;
-	};
-
-	class ItemOptics_Base;
-	class Mass1P69 : ItemOptics_Base {
+	//////////////////////////////////////////////////////////////////////////// OPTICS
+	class ItemOptics;
+	class Mass1P69 : ItemOptics {
 		s_showEnterMisalignment = 1;
 		s_isFullscreen = 1;
-		// no PIP for this :(
+		s_pipRadius = 0.7;
+		s_pipMagnification = 0.1;
+		s_pipBlur = 0.05;
+		s_pipChromAber = 0.3;
 	};
-	class MassPKA : ItemOptics_Base {
+	class MassPKA : ItemOptics {
 		s_pipRadius = 0.6;
 		s_pipMagnification = 0.25;
 		s_pipBlur = 0.01;
 		s_pipChromAber = 0.1;
 	};
-	class MassScope : ItemOptics_Base {
+	class MassScope : ItemOptics {
 		s_pipRadius = 0.4;
-		s_pipMagnification = 0.4;
+		s_pipMagnification = 0.1;
 		s_pipBlur = 0.01;
 		s_pipChromAber = 0.2;
+		s_nearPlaneOverride = 0.3;
 	};
 	class LongrangeOptic;
 	class MassScoutLRS : LongrangeOptic {
 		s_showEnterMisalignment = 1;
 		s_isFullscreen = 1;
-		s_pipMagnification = 0.2;
+		s_pipMagnification = 0.1;
 		s_pipRadius = 1.0;
 		s_pipBlur = 0.01;
 		s_pipChromAber = 0.4;
 	};
+
+	//////////////////////////////////////////////////////////////////////////// BUTTSTOCKS
+
+	class Inventory_Base;
+	class M4_OEBttstck;
+	class Mass_HK416Bttstck : M4_OEBttstck {
+		s_recoilControlStabilityX = 0.40;
+		s_recoilControlStabilityY = 0.40;
+		s_recoilControlMisalignmentX = 0.05;
+		s_recoilControlMisalignmentY = 0.15;
+		s_recoilControlKick = 0.60;
+	};
+	class MassAR15AdjBttstck : M4_OEBttstck {
+		s_recoilControlStabilityX = 0.40;
+		s_recoilControlStabilityY = 0.40;
+		s_recoilControlMisalignmentX = 0.05;
+		s_recoilControlMisalignmentY = 0.15;
+		s_recoilControlKick = 0.60;
+	};
+	class MassM417Stock : M4_OEBttstck {
+		s_recoilControlStabilityX = 0.40;
+		s_recoilControlStabilityY = 0.40;
+		s_recoilControlMisalignmentX = 0.05;
+		s_recoilControlMisalignmentY = 0.15;
+		s_recoilControlKick = 0.60;
+	};
+	class MassSPAS12Stock : Inventory_Base {
+		s_recoilControlStabilityX = 0.40;
+		s_recoilControlStabilityY = 0.40;
+		s_recoilControlMisalignmentX = 0.05;
+		s_recoilControlMisalignmentY = 0.15;
+		s_recoilControlKick = 0.60;
+	};
+
+	//////////////////////////////////////////////////////////////////////////// COMPENSATORS
+
+	class ItemSuppressor;
+	class MassMuzzle338 : ItemSuppressor {
+		s_recoilControlStabilityX = 0.15;
+		s_recoilControlStabilityY = 0.05;
+		s_recoilControlMisalignmentX = 0.10;
+		s_recoilControlMisalignmentY = 0.05;
+	};
+	class MassMuzzle556 : ItemSuppressor {
+		s_recoilControlStabilityX = 0.15;
+		s_recoilControlStabilityY = 0.05;
+		s_recoilControlMisalignmentX = 0.10;
+		s_recoilControlMisalignmentY = 0.05;
+	};
+	class MassMuzzle762 : ItemSuppressor {
+		s_recoilControlStabilityX = 0.15;
+		s_recoilControlStabilityY = 0.05;
+		s_recoilControlMisalignmentX = 0.10;
+		s_recoilControlMisalignmentY = 0.05;
+	};
+	class MassMuzzleComp556 : ItemSuppressor {
+		s_recoilControlStabilityX = 0.15;
+		s_recoilControlStabilityY = 0.05;
+		s_recoilControlMisalignmentX = 0.10;
+		s_recoilControlMisalignmentY = 0.05;
+	};
+	class MassMuzzlePistol : ItemSuppressor {
+		s_recoilControlStabilityX = 0.15;
+		s_recoilControlStabilityY = 0.05;
+		s_recoilControlMisalignmentX = 0.10;
+		s_recoilControlMisalignmentY = 0.05;
+	};
+
 };
--- sGunplay_MMIO/scripts/4_World/recoils/recoils.c
+++ sGunplay_MMIO/scripts/4_World/recoils/recoils.c
@@ -1,14 +1,14 @@
 class MassPPSHRecoil : RecoilBase {
 	override void initRecoilParameters() {
-		handsRanges        = {-0.690, 0.690,  2.450,  3.750};	
+		handsRanges        = {-0.900,  0.900,  2.450,  3.750};	
 		handsAccumSpeed    = 0.975;
-		handsResetSpeed    = 0.650;
+		handsResetSpeed    = 0.730;
 		
-		misalignIntensity  = {0.250, 0.125};
+		misalignIntensity  = { 1.750,  0.225};
 		misalignAccumSpeed = 0.975;
-		misalignResetSpeed = 0.600;
+		misalignResetSpeed = 0.850;
 	
-		mouseRanges        = { 0.175, 0.250,  1.250,  1.750};
+		mouseRanges        = {-0.215,  0.215,  1.250,  1.750};
 		mouseTime          = 0.150;
 	
 		kick               = 0.065;
@@ -18,15 +18,15 @@ class MassPPSHRecoil : RecoilBase {
 
 modded class MassAR15DMRRecoil : RecoilBase {
 	override void initRecoilParameters() {
-		handsRanges        = {-1.550, 1.550,  2.750,  3.985};	
+		handsRanges        = {-1.750,  1.750,  2.750,  3.985};	
 		handsAccumSpeed    = 0.975;
 		handsResetSpeed    = 0.725;
 		
-		misalignIntensity  = {0.750, 0.750};
+		misalignIntensity  = { 1.550,  0.550};
 		misalignAccumSpeed = 0.925;
-		misalignResetSpeed = 0.700;
+		misalignResetSpeed = 0.850;
 	
-		mouseRanges        = { 0.175, 0.250,  1.250,  1.750};
+		mouseRanges        = {-0.215,  0.215,  1.250,  1.750};
 		mouseTime          = 0.150;
 	
 		kick               = 0.045;
@@ -36,15 +36,15 @@ modded class MassAR15DMRRecoil : RecoilBase {
 
 modded class MassM16Recoil : RecoilBase {
 	override void initRecoilParameters() {
-		handsRanges        = {-1.550, 1.550,  2.750,  3.985};	
-		handsAccumSpeed    = 0.920;
-		handsResetSpeed    = 0.800;
+		handsRanges        = {-1.750,  1.750,  2.750,  3.985};	
+		handsAccumSpeed    = 0.975;
+		handsResetSpeed    = 0.725;
 		
-		misalignIntensity  = {0.750, 0.750};
+		misalignIntensity  = { 1.550,  0.550};
 		misalignAccumSpeed = 0.925;
-		misalignResetSpeed = 0.700;
+		misalignResetSpeed = 0.850;
 	
-		mouseRanges        = { 0.175, 0.250,  1.250,  1.750};
+		mouseRanges        = {-0.215,  0.215,  1.250,  1.750};
 		mouseTime          = 0.150;
 	
 		kick               = 0.045;
@@ -72,141 +72,141 @@ modded class AN94Recoil : RecoilBase {
 
 modded class AN94BurstRecoil : RecoilBase {
 	override void initRecoilParameters() {
-		handsRanges        = {-1.550, 1.550,  2.750,  3.985};	
+		handsRanges        = {-1.550,  1.550,  2.750,  3.985};	
 		handsAccumSpeed    = 0.975;
 		handsResetSpeed    = 0.725;
 		
-		misalignIntensity  = {0.750, 0.750};
+		misalignIntensity  = { 1.755,  0.550};
 		misalignAccumSpeed = 0.950;
-		misalignResetSpeed = 0.650;
+		misalignResetSpeed = 0.850;
 	
-		mouseRanges        = { 0.175, 0.250,  1.250,  1.750};
+		mouseRanges        = {-0.215,  0.215,  1.250,  1.750};
 		mouseTime          = 0.150;
 	
-		kick               = 0.045;
+		kick               = 0.047;
 		kickResetTime      = 2.250;
 	}
 }
 
 modded class KivaariRecoil : RecoilBase {
 	override void initRecoilParameters() {
-		handsRanges        = { -1.165, 3.285,  7.565,  8.795};	
-		handsAccumSpeed    = 0.955;
-		handsResetSpeed    = 0.750;
+		handsRanges        = {-2.565,  2.585,  8.565,  9.595};	
+		handsAccumSpeed    = 0.980;
+		handsResetSpeed    = 0.720;
 
-		misalignIntensity  = {0.250, 0.250};
+		misalignIntensity  = { 1.750,  0.250};
 		misalignAccumSpeed = 0.955;
-		misalignResetSpeed = 0.500;
+		misalignResetSpeed = 0.870;
 	
-		mouseRanges        = { 0.175, 0.250,  1.750,  2.575};
+		mouseRanges        = {-0.215,  0.215,  1.750,  2.575};
 		mouseTime          = 0.200;
 	
-		kick               = 0.055;
-		kickResetTime      = 3.550;
+		kick               = 0.070;
+		kickResetTime      = 3.150;
 	}
 }
 
 modded class M417RecoilStanding : RecoilBase {
 	override void initRecoilParameters() {
-		handsRanges        = {-1.550, 1.550,  2.750,  3.985};	
-		handsAccumSpeed    = 0.975;
-		handsResetSpeed    = 0.725;
-		
-		misalignIntensity  = {0.750, 0.750};
-		misalignAccumSpeed = 0.925;
-		misalignResetSpeed = 0.700;
+		handsRanges        = {-2.565,  2.585,  8.565,  9.595};	
+		handsAccumSpeed    = 0.980;
+		handsResetSpeed    = 0.720;
+
+		misalignIntensity  = { 1.750,  0.250};
+		misalignAccumSpeed = 0.955;
+		misalignResetSpeed = 0.870;
 	
-		mouseRanges        = { 0.175, 0.250,  1.250,  1.750};
-		mouseTime          = 0.150;
+		mouseRanges        = {-0.215,  0.215,  1.750,  2.575};
+		mouseTime          = 0.200;
 	
-		kick               = 0.045;
-		kickResetTime      = 2.250;
+		kick               = 0.070;
+		kickResetTime      = 3.150;
 	}
 }
 
 modded class M417RecoilProne : RecoilBase {
 	override void initRecoilParameters() {
-		handsRanges        = {-1.550, 1.550,  2.750,  3.985};	
-		handsAccumSpeed    = 0.975;
-		handsResetSpeed    = 0.725;
-		
-		misalignIntensity  = {0.750, 0.750};
-		misalignAccumSpeed = 0.925;
-		misalignResetSpeed = 0.700;
+		handsRanges        = {-2.565,  2.585,  8.565,  9.595};	
+		handsAccumSpeed    = 0.980;
+		handsResetSpeed    = 0.720;
+
+		misalignIntensity  = { 1.750,  0.250};
+		misalignAccumSpeed = 0.955;
+		misalignResetSpeed = 0.870;
 	
-		mouseRanges        = { 0.175, 0.250,  1.250,  1.750};
-		mouseTime          = 0.150;
+		mouseRanges        = {-0.215,  0.215,  1.750,  2.575};
+		mouseTime          = 0.200;
 	
-		kick               = 0.045;
-		kickResetTime      = 2.250;
+		kick               = 0.070;
+		kickResetTime      = 3.150;
 	}
 }
 
 modded class M98BRecoilStanding : RecoilBase {
 	override void initRecoilParameters() {
-		handsRanges        = { -1.165, 3.285,  7.565,  8.795};	
-		handsAccumSpeed    = 0.955;
-		handsResetSpeed    = 0.750;
+		handsRanges        = {-2.565,  2.585,  8.550,  9.500};
+		handsAccumSpeed    = 0.980;
+		handsResetSpeed    = 0.720;
 
-		misalignIntensity  = {0.250, 0.250};
+		misalignIntensity  = { 1.750,  0.400};
 		misalignAccumSpeed = 0.955;
-		misalignResetSpeed = 0.500;
+		misalignResetSpeed = 0.870;
 	
-		mouseRanges        = { 0.175, 0.250,  1.750,  2.575};
+		mouseRanges        = {-0.215,  0.215,  1.750,  2.575};
 		mouseTime          = 0.200;
 	
-		kick               = 0.055;
-		kickResetTime      = 3.550;
+		kick               = 0.085;
+		kickResetTime      = 3.150;
 	}
 }
 
 modded class M98BRecoilCrouched : RecoilBase {
 	override void initRecoilParameters() {
-		handsRanges        = { -1.165, 3.285,  7.565,  8.795};	
-		handsAccumSpeed    = 0.955;
-		handsResetSpeed    = 0.750;
+		handsRanges        = {-2.565,  2.585,  8.550,  9.500};
+		handsAccumSpeed    = 0.980;
+		handsResetSpeed    = 0.720;
 
-		misalignIntensity  = {0.250, 0.250};
+		misalignIntensity  = { 1.750,  0.400};
 		misalignAccumSpeed = 0.955;
-		misalignResetSpeed = 0.500;
+		misalignResetSpeed = 0.870;
 	
-		mouseRanges        = { 0.175, 0.250,  1.750,  2.575};
+		mouseRanges        = {-0.215,  0.215,  1.750,  2.575};
 		mouseTime          = 0.200;
 	
-		kick               = 0.055;
-		kickResetTime      = 3.550;
+		kick               = 0.085;
+		kickResetTime      = 3.150;
 	}
 }
 
 modded class M98BRecoilProne : RecoilBase {
 	override void initRecoilParameters() {
-		handsRanges        = { -1.165, 3.285,  7.565,  8.795};	
-		handsAccumSpeed    = 0.955;
-		handsResetSpeed    = 0.750;
+		handsRanges        = {-2.565,  2.585,  8.550,  9.500};
+		handsAccumSpeed    = 0.980;
+		handsResetSpeed    = 0.720;
 
-		misalignIntensity  = {0.250, 0.250};
+		misalignIntensity  = { 1.750,  0.400};
 		misalignAccumSpeed = 0.955;
-		misalignResetSpeed = 0.500;
+		misalignResetSpeed = 0.870;
 	
-		mouseRanges        = { 0.175, 0.250,  1.750,  2.575};
+		mouseRanges        = {-0.215,  0.215,  1.750,  2.575};
 		mouseTime          = 0.200;
 	
-		kick               = 0.055;
-		kickResetTime      = 3.550;
+		kick               = 0.085;
+		kickResetTime      = 3.150;
 	}
 }
 
 modded class MassMP40Recoil : RecoilBase {
 	override void initRecoilParameters() {
-		handsRanges        = {-0.690, 0.690,  2.450,  3.750};	
+		handsRanges        = {-0.900,  0.900,  2.450,  3.750};	
 		handsAccumSpeed    = 0.975;
-		handsResetSpeed    = 0.650;
+		handsResetSpeed    = 0.730;
 		
-		misalignIntensity  = {0.250, 0.125};
+		misalignIntensity  = { 1.750,  0.225};
 		misalignAccumSpeed = 0.975;
-		misalignResetSpeed = 0.600;
+		misalignResetSpeed = 0.850;
 	
-		mouseRanges        = { 0.175, 0.250,  1.250,  1.750};
+		mouseRanges        = {-0.215,  0.215,  1.250,  1.750};
 		mouseTime          = 0.150;
 	
 		kick               = 0.065;
@@ -324,72 +324,72 @@ modded class RPK74RecoilStanding : RecoilBase {
 
 modded class RugerPrecisionRecoilStanding : RecoilBase {
 	override void initRecoilParameters() {
-		handsRanges        = { -1.165, 2.285,  7.565,  8.795};	
-		handsAccumSpeed    = 0.955;
-		handsResetSpeed    = 0.750;
+		handsRanges        = {-2.565,  2.585,  8.565,  9.595};	
+		handsAccumSpeed    = 0.980;
+		handsResetSpeed    = 0.720;
 
-		misalignIntensity  = {0.250, 0.250};
-		misalignAccumSpeed = 0.925;
-		misalignResetSpeed = 0.850;
+		misalignIntensity  = { 1.750,  0.250};
+		misalignAccumSpeed = 0.955;
+		misalignResetSpeed = 0.870;
 	
-		mouseRanges        = { 0.175, 0.250,  1.750,  2.575};
+		mouseRanges        = {-0.215,  0.215,  1.750,  2.575};
 		mouseTime          = 0.200;
 	
-		kick               = 0.085;
+		kick               = 0.070;
 		kickResetTime      = 3.150;
 	}
 }
 
 modded class RugerPrecisionRecoilCrouched : RecoilBase {
 	override void initRecoilParameters() {
-		handsRanges        = { -1.165, 2.285,  7.565,  8.795};	
-		handsAccumSpeed    = 0.955;
-		handsResetSpeed    = 0.750;
+		handsRanges        = {-2.565,  2.585,  8.565,  9.595};	
+		handsAccumSpeed    = 0.980;
+		handsResetSpeed    = 0.720;
 
-		misalignIntensity  = {0.250, 0.250};
-		misalignAccumSpeed = 0.925;
-		misalignResetSpeed = 0.850;
+		misalignIntensity  = { 1.750,  0.250};
+		misalignAccumSpeed = 0.955;
+		misalignResetSpeed = 0.870;
 	
-		mouseRanges        = { 0.175, 0.250,  1.750,  2.575};
+		mouseRanges        = {-0.215,  0.215,  1.750,  2.575};
 		mouseTime          = 0.200;
 	
-		kick               = 0.085;
+		kick               = 0.070;
 		kickResetTime      = 3.150;
 	}
 }
 
 modded class RugerPrecisionRecoilProne : RecoilBase {
 	override void initRecoilParameters() {
-		handsRanges        = { -1.165, 2.285,  7.565,  8.795};	
-		handsAccumSpeed    = 0.955;
-		handsResetSpeed    = 0.750;
+		handsRanges        = {-2.565,  2.585,  8.565,  9.595};	
+		handsAccumSpeed    = 0.980;
+		handsResetSpeed    = 0.720;
 
-		misalignIntensity  = {0.250, 0.250};
-		misalignAccumSpeed = 0.925;
-		misalignResetSpeed = 0.850;
+		misalignIntensity  = { 1.750,  0.250};
+		misalignAccumSpeed = 0.955;
+		misalignResetSpeed = 0.870;
 	
-		mouseRanges        = { 0.175, 0.250,  1.750,  2.575};
+		mouseRanges        = {-0.215,  0.215,  1.750,  2.575};
 		mouseTime          = 0.200;
 	
-		kick               = 0.085;
+		kick               = 0.070;
 		kickResetTime      = 3.150;
 	}
 }
 
 modded class MassSVURecoil : RecoilBase {
 	override void initRecoilParameters() {
-		handsRanges        = { -1.165, 3.285,  7.565,  8.795};	
-		handsAccumSpeed    = 0.955;
-		handsResetSpeed    = 0.750;
+		handsRanges        = {-2.565,  2.585,  8.550,  9.500};
+		handsAccumSpeed    = 0.980;
+		handsResetSpeed    = 0.720;
 
-		misalignIntensity  = {0.250, 0.250};
+		misalignIntensity  = { 1.750,  0.400};
 		misalignAccumSpeed = 0.955;
-		misalignResetSpeed = 0.500;
+		misalignResetSpeed = 0.870;
 	
-		mouseRanges        = { 0.175, 0.250,  1.750,  2.575};
+		mouseRanges        = {-0.215,  0.215,  1.750,  2.575};
 		mouseTime          = 0.200;
 	
-		kick               = 0.055;
-		kickResetTime      = 3.550;
+		kick               = 0.085;
+		kickResetTime      = 3.150;
 	}
 }
